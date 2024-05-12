0ebca73219a9eebaa5f2f95ca382bf39064c4787
Jeba3210 <jebaparvin2018@gmail.com>
2024-05-12 22:04:49 +0530

    Todo functionality

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -2,84 +2,147 @@ import React, { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-toastify';
 import { v4 as uuid } from 'uuid';
 import Task from './Task';
+import { MdEdit, MdOutlineAdd } from 'react-icons/md';
 
 function Profile() {
 
-    const task = useRef();
-    const description = useRef();
+  const task = useRef();
+  const description = useRef();
 
-  const [userDetails , setUserDetails] = useState();
- 
-  
+  const [userDetails, setUserDetails] = useState();
+
+  const [taskDetails, setTaskDetails] = useState([]);
+
+  const [toggleSubmit, setToggleSubmit] = useState(false);
+  const [editedId, setEditedId] = useState();
 
   useEffect(() => {
-  
+
     setUserDetails(JSON.parse(localStorage.getItem('details')));
 
-  },[])
+  }, [])
 
 
-  function handleTaskSubmit(e){
+  function implementTaskSubmit(e) {
     e.preventDefault();
 
-    const taskInput = task.current.value;
-    const descriptionInput = description.current.value;
+    var taskInput = task.current.value;
+    var descriptionInput = description.current.value;
 
     console.log(taskInput);
-    console.log(descriptionInput)
+    console.log(descriptionInput);
 
-    if(!taskInput || !descriptionInput){
+    if (!taskInput || !descriptionInput) {
       toast.error('All fields are mandatory!');
       return
+    } else if (taskInput && descriptionInput && toggleSubmit) {
+      setTaskDetails(taskDetails.map(task => {
+        if (task.key === editedId) {
+          return { ...task, task: taskInput, description: descriptionInput }
+        }
+        return task;
+
+      })
+      )
+
+      task.current.value = '';
+      description.current.value = '';
+
+      setToggleSubmit(false);
+      setEditedId('');
+
+    } else {
+      const taskDetailsWithKey = {
+        task: taskInput,
+        description: descriptionInput,
+        key: uuid()
+      }
+
+      // localStorage.setItem('taskDetails' , JSON.stringify(taskDetails));
+
+      setTaskDetails([...taskDetails, taskDetailsWithKey]);
+
+      task.current.value = '';
+      description.current.value = '';
+
     }
-
-    const taskDetails = {
-      task : taskInput,
-      description : descriptionInput,
-      key : uuid()
-    }
-
-    localStorage.setItem('taskDetails' , JSON.stringify(taskDetails));
-
-    
 
   }
-  
+
+  // Delete Task
+  function deleteTask(id) {
+    setTaskDetails(taskDetails.filter(task => id !== task.key));
+
+  }
+
+  // Edit Task
+  function editTask(id) {
+    const editedTask = taskDetails.find(task => { return id === task.key });
+    // console.log(editedTask)
+    task.current.value = editedTask.task;
+    description.current.value = editedTask.description;
+
+    setToggleSubmit(true);
+    setEditedId(id);
+    console.log(editedId);
+
+
+  }
 
   return (
     <div>
-     {userDetails && 
-     <div>
-     <img src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" alt="" />
-     <p>Welcome {userDetails.name}</p>
-   </div>
-     }
-     <div>
-      <form action="#" onSubmit={handleTaskSubmit}>
-        <label htmlFor="task">Your task</label>
-              <input type="text"
-                     id='task'
-                     placeholder='Enter your task'
-                     ref={task}
-              />
-        <label htmlFor="desc">Description</label>
-              <input type="text"
-                     id='desc'
-                     placeholder='Describe your task'
-                     ref={description}
-              />
-        <button type='submit'>Submit</button>
-      </form>
-     </div>
-     {localStorage.getItem('taskDetails') &&  
-     <div>
-      <Task />
-     </div>
-     
-     }
-        
-    
-      
+      {userDetails &&
+        <div>
+          <img src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg" alt="" />
+          <p>Welcome {userDetails.name}</p>
+        </div>
+      }
+      <div>
+        <form action="#" onSubmit={implementTaskSubmit}>
+          <label htmlFor="task">Your task</label>
+          <input type="text"
+            id='task'
+            placeholder='Enter your task'
+            ref={task}
+          />
+          <label htmlFor="desc">Description</label>
+          <input type="text"
+            id='desc'
+            placeholder='Describe your task'
+            ref={description}
+          />
+
+          {!toggleSubmit ?
+            <button type='submit'><MdOutlineAdd /></button> :
+            <button type='submit'><MdEdit /></button>
+          }
+
+        </form>
+      </div>
+
+      {
+        taskDetails &&
+        <div>
+          <ul>
+            {taskDetails.map((task,) => {
+              return (
+                <Task key={task.key}
+                  id={task.key}
+                  task={task.task}
+                  description={task.description}
+                  deleteTask={deleteTask}
+                  editTask={editTask}
+                />
+              )
+            }
+            )}
+          </ul>
+        </div>
+      }
+
+
+
+
     </div>
   )
 }
--- src/Profile/Task.jsx
+++ src/Profile/Task.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react'
+import { MdDelete , MdEdit } from "react-icons/md";
 
-function Task() {
+function Task({task , description , id , deleteTask, editTask}) { 
 
-    const [taskDescription , setTaskDescription] = useState();
-
-    useEffect(() => {
-        setTaskDescription(JSON.parse(localStorage.getItem('taskDetails')));
-    },[])
-
-    console.log(taskDescription);
+  
 
   return (
     <div>
-        <p>task</p>
+        <div>
+          Task : {task}
+        </div>
+        <div>
+          Description : {description}
+        </div>
+        <div><button className='bg-Amber-500' onClick={() => editTask(id)}><MdEdit /></button></div>
+        <div ><button className='bg-Amber-500' onClick={() => deleteTask(id)}><MdDelete /></button></div>
+        <div></div>
     </div>
   )
 }
